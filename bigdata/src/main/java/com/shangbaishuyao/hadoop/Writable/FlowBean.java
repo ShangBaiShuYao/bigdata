0fe4f05e1603e356b7fa09a357ebf3b581fc8bbe
shangbaishuyao <shangbaishuyao@163.com>
2021-02-20 16:36:48 +0800

    需求: 统计每一个手机号耗费的总上行流量、下行流量、总流量

---
+++ bigdata/src/main/java/com/shangbaishuyao/hadoop/Writable/FlowBean.java
@@ -0,0 +1,89 @@
+package com.shangbaishuyao.hadoop.Writable;
+
+import org.apache.hadoop.io.Writable;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.io.IOException;
+
+/**
+ * Desc:
+ * create by shangbaishuyao on 2021/2/20
+ * @Author: 上白书妖
+ * @Date: 15:56 2021/2/20
+ */
+
+// 1 实现writable序列化接口
+public class FlowBean implements Writable{
+
+    private long upFlow;
+    private long downFlow;
+    private long sumFlow;
+
+    //2  反序列化时，需要反射调用空参构造函数，所以必须有
+    public FlowBean() {
+        super();
+    }
+
+    public FlowBean(long upFlow, long downFlow) {
+        super();
+        this.upFlow = upFlow;
+        this.downFlow = downFlow;
+        this.sumFlow = upFlow + downFlow;
+    }
+
+    //3  写序列化方法
+    public void write(DataOutput out) throws IOException {
+        out.writeLong(upFlow);
+        out.writeLong(downFlow);
+        out.writeLong(sumFlow);
+    }
+
+    //4 反序列化方法
+    //5 反序列化方法读顺序必须和写序列化方法的写顺序必须一致
+    public void readFields(DataInput in) throws IOException {
+        this.upFlow  = in.readLong();
+        this.downFlow = in.readLong();
+        this.sumFlow = in.readLong();
+    }
+
+    // 6 编写toString方法，方便后续打印到文本
+    @Override
+    public String toString() {
+        return upFlow + "\t" + downFlow + "\t" + sumFlow;
+    }
+
+    public long getUpFlow() {
+        return upFlow;
+    }
+
+    public void setUpFlow(long upFlow) {
+        this.upFlow = upFlow;
+    }
+
+    public long getDownFlow() {
+        return downFlow;
+    }
+
+    public void setDownFlow(long downFlow) {
+        this.downFlow = downFlow;
+    }
+
+    public long getSumFlow() {
+        return sumFlow;
+    }
+
+    public void setSumFlow(long sumFlow) {
+        this.sumFlow = sumFlow;
+    }
+
+    public void set(long downFlow, long upFlow) {
+
+    }
+    public void get(long downFlow, long upFlow) {
+
+    }
+    public void setDownFLow(long parseLong) {
+    }
+    public void getDownFLow(long parseLong) {
+    }
+}
