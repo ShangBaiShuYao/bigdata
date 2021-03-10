5064d0d422b02dfede90d32a7d96f74a5c19efd7
shangbaishuyao <shangbaishuyao@163.com>
2021-03-10 15:31:16 +0800

    测试SparkSQL由RDD转换为DF

---
+++ bigdata/sparkSql/src/main/scala/com/shangbaishuyao/bean/People.scala
@@ -0,0 +1,8 @@
+package com.shangbaishuyao.bean
+/**
+ * Desc: 创建样例类 <br/>
+ * create by shangbaishuyao on 2021/3/10
+ * @Author: 上白书妖
+ * @Date: 15:07 2021/3/10
+ */
+case class People(name: String, age: Int)
