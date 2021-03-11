cc76401cfd3143d10109cba1c8497c876b6d58eb
shangbaishuyao <shangbaishuyao@163.com>
2021-03-11 14:58:12 +0800

    需求分析四: 各区域点击量Top3 商品

--- bigdata/sparkGmall/sparkmall-offline/src/main/scala/com/shangbaishuyao/app/AreaProductTop10App.scala
+++ bigdata/sparkGmall/sparkmall-offline/src/main/scala/com/shangbaishuyao/app/AreaProductTop10App.scala
@@ -5,25 +5,39 @@ import java.util.Properties
 import com.shangbaishuyao.udf.CityRatioUDAF
 import com.shangbaishuyao.utils.PropertiesUtil
 import org.apache.spark.sql.{DataFrame, SaveMode, SparkSession}
-
+/**
+ * Desc: 需求分析四: 各区域点击量Top3 商品 <br/>
+ *
+ * 需求分析: 按大区计算商品被点击的次数前3名，并计算出前2名占比及其他城市综合占比，结果数据展示：
+ *
+ * 地区	商品名称 	点击次数		 城市备注
+ * 华北	商品A	    100000		 北京21.2%，天津13.2%，其他65.6%
+ * 华北	商品P	    80200		   北京63.0%，太原10%，其他27.0%
+ * 华北	商品M	    40000		   北京63.0%，太原10%，其他27.0%
+ *
+ * create by shangbaishuyao on 2021/3/11
+ *
+ * @Author: 上白书妖
+ * @Date: 12:09 2021/3/11
+ */
 object AreaProductTop10App {
 
   def main(args: Array[String]): Unit = {
+    //创建sparkSession
+    val spark: SparkSession = SparkSession
+                                .builder()
+                                .appName("AreaProductTop10App")
+                                .master("local[*]")
+                                .enableHiveSupport() //添加对hive的支持
+                                .getOrCreate()
 
-    //1.创建SparkSession
-    val spark: SparkSession = SparkSession.builder()
-      .master("local[*]")
-      .appName("AreaProductTop10App")
-      .enableHiveSupport()
-      .getOrCreate()
-
-    //2.导入隐式转换
+    //导入隐式转换
     import spark.implicits._
 
     //注册UDAF函数
-    spark.udf.register("cityRatio", CityRatioUDAF)
+    spark.udf.register("cityRatio",CityRatioUDAF)
 
-    //3.获取用户点击行为数据
+    //获取用户点击行为数据
     spark.sql("select click_product_id,city_id from user_visit_action where click_product_id>0").createTempView("uv")
 
     //4.获取点击商品城市，大区以及商品名称
@@ -52,10 +66,8 @@ object AreaProductTop10App {
       .option("dbtable", "area_count_info")
       .mode(SaveMode.Append)
       .save()
-
     //9.关闭资源
     spark.stop()
-
 
   }
 
--- bigdata/sparkGmall/sparkmall-offline/src/main/scala/com/shangbaishuyao/udf/CityRatioUDAF.scala
+++ bigdata/sparkGmall/sparkmall-offline/src/main/scala/com/shangbaishuyao/udf/CityRatioUDAF.scala
@@ -4,89 +4,69 @@ import com.shangbaishuyao.bean.CityRatio
 import org.apache.spark.sql.Row
 import org.apache.spark.sql.expressions.{MutableAggregationBuffer, UserDefinedAggregateFunction}
 import org.apache.spark.sql.types._
-
+/**
+ * Desc: 自定义函数 , 操作计算 各个城市占比 <br/>
+ * create by shangbaishuyao on 2021/3/11
+ * @Author: 上白书妖
+ * @Date: 12:33 2021/3/11
+ */
 object CityRatioUDAF extends UserDefinedAggregateFunction {
 
+  //前三个是做初始化的
   //输入：城市名
   override def inputSchema: StructType = StructType(StructField("cityName", StringType) :: Nil)
-
   //中间缓存：城市->点击次数
   override def bufferSchema: StructType = StructType(StructField("cityCount", MapType(StringType, LongType)) :: Nil)
-
   //输出：北京21.2%，天津13.2%，其他65.6%
   override def dataType: DataType = StringType
-
   //函数稳定性
   override def deterministic: Boolean = true
-
   //buffer初始化
   override def initialize(buffer: MutableAggregationBuffer): Unit = {
-
     buffer(0) = Map[String, Long]()
-
   }
-
   //区内累加
   override def update(buffer: MutableAggregationBuffer, input: Row): Unit = {
-
     //取出缓存中的数据
     val cityToCount: Map[String, Long] = buffer.getAs[Map[String, Long]](0)
-
     //判断传入的城市是否存在
     if (!input.isNullAt(0)) {
-
       //存在，获取城市名称
       val city: String = input.getString(0)
-
       //对获取的数据进行累加更新原缓存中的数据
       buffer(0) = cityToCount + (city -> (cityToCount.getOrElse(city, 0L) + 1L))
     }
   }
-
   //区间累加数据
   override def merge(buffer1: MutableAggregationBuffer, buffer2: Row): Unit = {
-
     //第一个分区中的缓存数据
     val map1: Map[String, Long] = buffer1.getAs[Map[String, Long]](0)
-
     //第二个分区中的缓存数据
     val map2: Map[String, Long] = buffer2.getAs[Map[String, Long]](0)
-
     //将分区的数据进行结合
-    buffer1(0) = map1.foldLeft(map2) { case (map, (city, count)) =>
-
-      map + (city -> (map.getOrElse(city, 0L) + count))
-
+    buffer1(0) = map1.foldLeft(map2) {
+      case (map, (city, count)) => map + (city -> (map.getOrElse(city, 0L) + count))
     }
-
   }
-
   //最终的计算---buffer:北京->50，天津->23..
   override def evaluate(buffer: Row): String = {
-
     //取出缓存中的城市及点击次数
     val cityToCount: Map[String, Long] = buffer.getAs[Map[String, Long]](0)
-
     //总点击次数
     var total_count = 0L
     cityToCount.foreach { case (_, count) => total_count += count }
-
     //排序取点击次数前2名的城市
     val cityToCountTop2: List[(String, Long)] = cityToCount.toList.sortWith(_._2 > _._2).take(2)
-
     //其他城市总占比
     var otherRatio = 100D
-
     //求出前2名的城市占比
-    val result: List[CityRatio] = cityToCountTop2.map { case (city, count) =>
-      val cityRatio: Double = Math.round(count * 10000D / total_count) / 100D
-      otherRatio -= cityRatio
-      CityRatio(city, cityRatio)
+    val result: List[CityRatio] = cityToCountTop2.map {
+      case (city, count) => val cityRatio: Double = Math.round(count * 10000D / total_count) / 100D
+        otherRatio -= cityRatio
+        CityRatio(city, cityRatio)
     }
-
     //将其他城市总占比加入该集合
     val ratios: List[CityRatio] = result :+ CityRatio("其他", Math.round(otherRatio * 10D) / 10D)
-
     //北京21.2%，天津13.2%，其他65.6%
     ratios.mkString(",")
   }
