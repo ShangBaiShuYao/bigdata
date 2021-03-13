b3ad5c78383cc3506ed51e143a916a3e183165f3
shangbaishuyao <shangbaishuyao@163.com>
2021-03-13 12:39:25 +0800

    使用连接池创建对redis的连接

--- bigdata/sparkGmall/sparkmall-online/src/main/scala/com/shangbaishuyao/utils/MyKafkaUtil.scala
+++ bigdata/sparkGmall/sparkmall-online/src/main/scala/com/shangbaishuyao/utils/MyKafkaUtil.scala
@@ -7,7 +7,12 @@ import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.InputDStream
 import org.apache.spark.streaming.kafka.KafkaUtils
-
+/**
+ * Desc: 读取kafka数据的工具类 <br/>
+ * create by shangbaishuyao on 2021/3/12
+ * @Author: 上白书妖
+ * @Date: 21:24 2021/3/12
+ */
 object MyKafkaUtil {
 
   //读取配置信息
@@ -20,15 +25,13 @@ object MyKafkaUtil {
   private val kafkaPara: Map[String, String] = Map[String, String](
     ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG -> brokers,
     ConsumerConfig.GROUP_ID_CONFIG -> groupID,
-    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> deserializer,
+    ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG -> deserializer, //反序列化的类
     ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG -> deserializer
   )
 
   def getKafkaDStream(ssc: StreamingContext, topics: Set[String]): InputDStream[(String, String)] = {
-
     //获取Kafka数据创建流
     val kafkaDStream: InputDStream[(String, String)] = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaPara, topics)
-
     kafkaDStream
   }
 
