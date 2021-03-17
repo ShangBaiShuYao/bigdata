e54771bef3cf850e8893306b957ec4ec606f2b8c
shangbaishuyao <shangbaishuyao@163.com>
2021-03-17 09:38:41 +0800

    添加前端数据展示模块

---
+++ dw-chart/src/main/java/com/shangbaishuyao/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.shangbaishuyao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class DemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
